2e0dbbdfd5d3262d6e372c5792a6f66e13febdf9
prathamagre <prathamvaishu@gmail.com>
2025-12-30 00:15:22 +0530

    added authentication to middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+const protect = (req, res, next) => {
+    let token = req.headers.authorization;
+    if (token && token.startsWith('Bearer ')) {
+        try {
+            token = token.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decoded;
+            next();
+        } catch (error) {
+            return res.status(401).json({ message: "Not authorized"});
+        }
+    } else {
+        return res.status(401).json({ message: "no token"});
+    }
+};
+
+export default protect;
